bf5a72aa7634412ab704611373a72fc1fc474d6b
Aurélien Goutsmedt <agoutsmedt@hotmail.fr>
2022-12-10 12:07:36 +0100

    Initial commit

--- render_cv.R
+++ render_cv.R
@@ -1,4 +1,7 @@
+data_path <- "https://docs.google.com/spreadsheets/d/13riQiHaGL4pte_lobGgRBAO2t_XeQ1wbVOBa-ZpId4s/edit?usp=sharing"
+source("build_ref.R")
+
 for(language in c("fr", "en")){
   rmarkdown::render(here::here("build_cv.Rmd"),
-                    here::here("cv_goutsmedt_{language}.pdf"))
+                    output_file = here::here(glue::glue("cv_goutsmedt_{language}.pdf")))
 }
