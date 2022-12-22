ce5beeeb62b536cedb724e37bf9ce44ff6ec3281
Aurélien Goutsmedt <agoutsmedt@hotmail.fr>
2022-12-22 18:20:02 +0100

    Finishing CV

--- build_ref.R
+++ build_ref.R
@@ -1,17 +1,23 @@
 library(bib2df)
+library(googlesheets4)
+
 references <- bib2df("bibliography.bib") %>% 
   janitor::clean_names() %>% 
   mutate(journal = ifelse(category == "PHDTHESIS", school, journal),
          journal = ifelse(category == "MISC", "R package", journal),
          doi = ifelse(category == "MISC", annotation, doi),
          annotation = ifelse(category == "MISC", NA, annotation),
-         type = str_extract(keywords, "ongoing-pub|published|other-pub|packages")) %>% 
-  select(category, bibtexkey, author, title, booktitle, chapter, journal, year, month, volume, number, pages, doi, annotation, type) %>% 
+         type = str_extract(keywords, "ongoing-pub|published|other-pub|invited-paper|packages"),
+         cv_type = str_extract(keywords, "short-cv|long-cv") %>% 
+           str_remove("-cv")) %>% 
+  select(category, bibtexkey, author, title, booktitle, chapter, journal, year, month, volume, number, pages, doi, annotation, type, cv_type) %>% 
   unnest_longer(author) %>% 
   mutate(across(all_of(c("author", "title", "journal")), ~str_remove_all(., "\\{|\\}")),
          across(all_of(c("author", "title", "journal")), ~str_replace_all(., "\\\\'e", "é")),
          across(all_of(c("author", "title", "journal")), ~str_replace_all(., "\\\\'o", "ó")),
-         across(all_of(c("author", "title", "journal")), ~str_replace_all(., "\\\\`a", "à"))) %>% 
+         across(all_of(c("author", "title", "journal")), ~str_replace_all(., "\\\\`a", "à")),
+         title_linked = ifelse(! is.na(doi), paste0("\\href{", doi, "}{", title, "}"), title),
+         preprint = ifelse(annotation !="", paste0("\\href{", annotation, "}{Preprint}"), NA)) %>% 
   mutate(first_name = str_extract(author, "(?<=, ).*"),
          surname = str_extract(author, ".*(?=,)"),
          .before = author) %>% 
@@ -34,11 +40,16 @@ mutate(author = ifelse(rownumber > 1 & lang == "en",
        info_pub = ifelse(! is.na(number), paste0(volume, "(", number, "): ", pages), volume),
        reference = paste0(author, ". (",
                           year, "). ",
-                          title, ". *",
+                          title_linked, ". *",
                           journal, "*"),
        reference = ifelse(! is.na(info_pub), 
                           paste0(reference, ", ", info_pub, "."),
-                          paste0(reference, ".")))
+                          paste0(reference, ".")),
+       reference = ifelse(! is.na(preprint),
+                          paste0(reference, " (", preprint, ")"),
+                          reference)) %>% 
+  select(-c(title_linked, preprint, rownumber, info_pub))
 
 references_complete %>% 
+  arrange(desc(year), bibtexkey) %>% 
   sheet_write(data_path, sheet = "Publications")
--- render_cv.R
+++ render_cv.R
@@ -1,7 +1,12 @@
+library(tidyverse)
 data_path <- "https://docs.google.com/spreadsheets/d/13riQiHaGL4pte_lobGgRBAO2t_XeQ1wbVOBa-ZpId4s/edit?usp=sharing"
 source("build_ref.R")
 
+cnrs_rank <- TRUE
 for(language in c("fr", "en")){
-  rmarkdown::render(here::here("build_cv.Rmd"),
-                    output_file = here::here(glue::glue("cv_goutsmedt_{language}.pdf")))
+  for(cv in c("short", "long")){
+    rmarkdown::render(here::here("build_cv.Rmd"),
+                      output_file = here::here(glue::glue("cv_{cv}_goutsmedt_{language}.pdf")))
+  }
 }
+
